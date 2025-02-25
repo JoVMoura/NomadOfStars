9b1d844c8cc3293b0da8133c4e0d1a34021267ea
FelipeSilvestre04 <132397300+FelipeSilvestre04@users.noreply.github.com>
2025-02-24 23:52:58 -0300

    pause

--- NomadOfStars/Assets/Scripts/UI_control.cs
+++ NomadOfStars/Assets/Scripts/UI_control.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 public class UI_control : MonoBehaviour
 {
     [SerializeField] private GameObject menuBuild;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject menuPause;
+    [SerializeField] private GameObject menuDerrota;
+    [SerializeField] private GameObject menuVitoria;
+    [SerializeField] private GameObject menurBotoes;
 
     public void AbriBuild()
     {
@@ -17,5 +16,41 @@ public class UI_control : MonoBehaviour
     public void FecharBuild()
     {
         menuBuild.SetActive(false);
+    }
+
+    public void AbrirPause()
+    {
+        menuPause.SetActive(true);
+        menurBotoes.SetActive(true);
+    }
+
+    public void FecharPause()
+    {
+        menuPause.SetActive(false);
+        menurBotoes.SetActive(false);
+    }
+
+    public void AbrirDerrota()
+    {
+        menuDerrota.SetActive(true);
+        menurBotoes.SetActive(true);
+    }
+
+    public void FecharDerrota()
+    {
+        menuDerrota.SetActive(false);
+        menurBotoes.SetActive(false);
+    }
+
+    public void AbrirVitoria()
+    {
+        menuVitoria.SetActive(true);
+        menurBotoes.SetActive(true);
+    }
+
+    public void FecharVitoria()
+    {
+        menuVitoria.SetActive(false);
+        menurBotoes.SetActive(false);
     }
 }
