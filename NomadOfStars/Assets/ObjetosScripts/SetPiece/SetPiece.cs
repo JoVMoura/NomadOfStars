13361c420aa6d44bfe2eb25e55f772abafdb05d5
JoVi Moura <jovitorprof@gmail.com>
2025-07-09 20:34:05 -0300

    Muita coisa gente

---
+++ NomadOfStars/Assets/ObjetosScripts/SetPiece/SetPiece.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "SetPiece", menuName = "Scriptable Objects/SetPiece")]
+public class SetPiece : ScriptableObject
+{
+    public string setPieceName;
+    public List<LocalRangeData> localRange;
+    [Space(10f)]
+    [Header("TileMap")]
+    public bool preSetTile;
+    public List<Tile> tiles;
+    [Space(10f)]
+    [Header("Structures")]
+    public List<StructuresData> structure;
+}
+
+[Serializable]
+public class StructuresData
+{
+    [field: SerializeField] public GameObject prefab { get; private set; }
+    [field: SerializeField] public int minAmount { get; private set; }
+    [field: SerializeField] public int maxAmount { get; private set; }
+    [field: SerializeField] public float marginX { get; private set; }
+    [field: SerializeField] public float marginY { get; private set; }
+    [field: SerializeField] public bool center { get; private set; }
+}
+
+[Serializable]
+public class LocalRangeData
+{
+
+    [field: SerializeField] public int minX { get; private set; }
+    [field: SerializeField] public int maxX { get; private set; }
+    [field: SerializeField] public int minY { get; private set; }
+    [field: SerializeField] public int maxY { get; private set; }
+}
---
+++ NomadOfStars/Assets/ObjetosScripts/World/World.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "World", menuName = "Scriptable Objects/World")]
+public class World : ScriptableObject
+{
+    public int size;
+    public List<SetPieceData> setpieces;
+}
+
+[Serializable]
+public class SetPieceData
+{
+    [field: SerializeField] public SetPiece piece { get; private set; }
+    [field: SerializeField] public bool essential { get; private set; }
+    [field: Range(1, 100)][field: SerializeField] public int rarity { get; private set; }
+
+    [field: SerializeField] public bool AmountRange { get; private set; }
+    [field: SerializeField] public int minAmount { get; private set; }
+    [field: SerializeField] public int maxAmount { get; private set; }
+}
+
