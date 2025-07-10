ee86f3893802001ca2d47d3ff5bd67731c9a9ebd
FelipeSilvestre04 <132397300+FelipeSilvestre04@users.noreply.github.com>
2025-07-09 23:19:15 -0300

    sprites

--- NomadOfStars/Assets/Scenes/manager.cs
+++ NomadOfStars/Assets/Scenes/manager.cs
@@ -5,7 +5,7 @@ public class MenuManager : MonoBehaviour
 {
     public void Jogar()
     {
-        SceneManager.LoadScene("Jogo"); // Altere para o nome correto da sua cena
+        SceneManager.LoadScene("Intro"); // Altere para o nome correto da sua cena
     }
 
     public void Sair()
