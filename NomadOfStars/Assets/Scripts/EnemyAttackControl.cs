182019c73476e847bef0a0e8cf7d2d9007e5aa1f
FelipeSilvestre04 <132397300+FelipeSilvestre04@users.noreply.github.com>
2025-07-24 12:36:00 -0300

    final

--- NomadOfStars/Assets/Scripts/EnemyAttackControl.cs
+++ NomadOfStars/Assets/Scripts/EnemyAttackControl.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class EnemyAttackControl : MonoBehaviour
 {
     [SerializeField]private EnemyControl enemyControl;
+    [SerializeField] private string tag_nave;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Tower" || collision.gameObject.tag == "Base")
+        if (collision.gameObject.tag == "Tower" || collision.gameObject.tag == tag_nave)
         {
             enemyControl.AttackTarget(collision.gameObject);
         }
